84f05ab023a95a92d7e069fd159b83e7c2b45fa0
Karina <kbrilhante@gmail.com>
2024-09-18 15:15:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,14 @@ r"""
 """
 ]
 
+# this will return the index of the player's choice (notice the -1)
 player = int(input("What is our pick?\n1 - Rock\n2 - Paper\n3 - Scissors\n> ")) - 1
 
 # DO NOT ERASE ROWS ABOVE!
 # Build a Rock, Paper, Scissors
+
+# TODO: show the player's choice on screen (tip: you can use the ASCII codes to draw it)
+# TODO: get the computer's choice
+# TODO: show the computer's choice on screen
+# TODO: determine who won the match
+# TODO: show the winner on screen
